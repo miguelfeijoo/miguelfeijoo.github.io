beabce7765eab2e5324ed1cc7e88acf0eceeb005
Miguel Feijoo <ma.feijoo1120@uniandes.edu.co>
2018-09-23 00:02:59 -0500

    changes4

--- IncautacionesColombia2018/js/grouped_bar_chart_1.js
+++ IncautacionesColombia2018/js/grouped_bar_chart_1.js
@@ -1,10 +1,10 @@
    
-  cantidad_incautaciones_mes=d3.csv('https://raw.githubusercontent.com/miguelfeijoo/IncautacionesColombia2018/master/DATOS/CANTIDAD_INCAUTADA.csv', function ( d ) {
+  var cantidad_incautaciones_mes=d3.csv('https://raw.githubusercontent.com/miguelfeijoo/IncautacionesColombia2018/master/DATOS/CANTIDAD_INCAUTADA.csv', function ( d ) {
   return {
     'MES': d[ 'MES' ],
     'BASE DE COCA': +d[ 'BASE DE COCA' ],
     'BASUCO': +d[ 'BASUCO' ],
-    'COCAINA': d[ 'COCAINA' ],
+    'COCAINA': +d[ 'COCAINA' ],
     'HEROINA': +d[ 'HEROINA' ],
     'MARIHUANA': +d[ 'MARIHUANA' ]
     
