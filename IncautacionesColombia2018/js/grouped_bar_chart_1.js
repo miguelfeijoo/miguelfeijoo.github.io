f287dd10eed51a1ac7db0f6ea73fae8c34a2dc5e
Miguel Feijoo <ma.feijoo1120@uniandes.edu.co>
2018-09-23 00:11:12 -0500

    changes5

--- IncautacionesColombia2018/js/grouped_bar_chart_1.js
+++ IncautacionesColombia2018/js/grouped_bar_chart_1.js
@@ -1,5 +1,5 @@
    
-  var cantidad_incautaciones_mes=d3.csv('https://raw.githubusercontent.com/miguelfeijoo/IncautacionesColombia2018/master/DATOS/CANTIDAD_INCAUTADA.csv', function ( d ) {
+  var cantidad_incautaciones_mes=d3.csv('data/CANTIDAD_INCAUTADA.csv', function ( d ) {
   return {
     'MES': d[ 'MES' ],
     'BASE DE COCA': +d[ 'BASE DE COCA' ],
@@ -10,6 +10,8 @@
     
   };
 } )
+
+  console.log(cantidad_incautaciones_mes)
 
   var svg = d3.select( DOM.svg( width, 500 ) ),
     margin = { top: 50, right: 30, bottom: 30, left: 80 },
