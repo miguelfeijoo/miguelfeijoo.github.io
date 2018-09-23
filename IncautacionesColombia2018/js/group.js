8acd21a726d75b6af353198bb2af2908f655a39d
Miguel Feijoo <ma.feijoo1120@uniandes.edu.co>
2018-09-23 01:30:41 -0500

    no comment 8

--- IncautacionesColombia2018/js/group.js
+++ IncautacionesColombia2018/js/group.js
@@ -1,5 +1,5 @@
 function first(){
-//d3 = require("d3", "d3-svg-legend");
+d3 = require("d3", "d3-svg-legend");
 cantidad_incautaciones_mes=d3.csv('https://raw.githubusercontent.com/miguelfeijoo/IncautacionesColombia2018/master/DATOS/CANTIDAD_INCAUTADA.csv', function ( d ) {
   return {
     'MES': d[ 'MES' ],
@@ -11,6 +11,8 @@ cantidad_incautaciones_mes=d3.csv('https://raw.githubusercontent.com/miguelfeijo
     
   };
 } );
+
+console.log(cantidad_incautaciones_mes)
 
 
 var svg = d3.select( '#d3_01_grafico' ),
