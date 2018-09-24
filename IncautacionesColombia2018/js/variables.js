3154ebd5d299356d940ce918ae989310ce037ce3
Miguel Feijoo <ma.feijoo1120@uniandes.edu.co>
2018-09-23 23:04:57 -0500

    return

--- IncautacionesColombia2018/js/variables.js
+++ IncautacionesColombia2018/js/variables.js
@@ -1,6 +1,2 @@
-console.log(":::::::::::::::::::::::::::::::::::::::::")
-console.log("::: Desarrollado por Jairo Ruiz Saenz :::")
-console.log(":::::::::::::::::::::::::::::::::::::::::")
-
 var WIDTH = 1140,
     HEIGHT = 500;
