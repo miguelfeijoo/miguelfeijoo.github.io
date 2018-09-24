d3f471685d8e10d6cc25740d282b9c8e5ed57549
Miguel Feijoo <ma.feijoo1120@uniandes.edu.co>
2018-09-23 19:30:23 -0500

    ticks pop error solve 10

--- IncautacionesColombia2018/js/group.js
+++ IncautacionesColombia2018/js/group.js
@@ -90,8 +90,9 @@ shuffle(colors);
   
   var z = d3.scaleOrdinal( colors.slice(0,n));
     
-  console.log("keysssssss    "+keys.then(function(data) {data}).then(d=>d));
-
+console.log("pepeeeeee    " +keys.then(function(data) {
+    console.log('all is completed', data);
+}))
 
   x0.domain( cantidad_incautaciones_mes.then(function(data) {data.map( d => d[ 'MES' ] )} ));
   x1.domain( keys ).rangeRound( [ 0, x0.bandwidth() ] );
