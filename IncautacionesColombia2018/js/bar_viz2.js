bed00fa6c462b119d14ac38127c3563b5b436301
Miguel Feijoo <ma.feijoo1120@uniandes.edu.co>
2018-09-25 15:27:36 -0500

    nuevo

--- IncautacionesColombia2018/js/bar_viz2.js
+++ IncautacionesColombia2018/js/bar_viz2.js
@@ -1,3 +1,9 @@
+function removegraph(svg){
+
+svg.html("")
+
+}
+
 function grafica(depto,incautaciones_depto_sitio_mes){
 
 
@@ -171,15 +177,6 @@ shuffle(colors)
 
 function second(){
 
-var svg = d3.select( "svg.d" ),
-    margin = { top: 50, right: 30, bottom: 70, left: 80 },
-    iwidth = +svg.attr( "width" ) - margin.left - margin.right,
-    iheight = +svg.attr( "height" ) - margin.top - margin.bottom,
-    g = svg.append( "g" ).attr( "transform", "translate(" + margin.left + "," + margin.top + ")" );
-
-svg.html("")
-
-
 d3.csv('https://raw.githubusercontent.com/miguelfeijoo/IncautacionesColombia2018/master/DATOS/INCAUTACIONES_DEPARTAMENTO.csv', function ( d ) {
   return {
     'DEPARTAMENTO': d[ 'DEPARTAMENTO' ],
@@ -196,6 +193,7 @@ d3.csv('https://raw.githubusercontent.com/miguelfeijoo/IncautacionesColombia2018
   d3.select("#depto").on("change", function(){
                 depto = this.value;
 
+ removegraph(svg);
   grafica(depto,incautaciones_depto_sitio_mes)
 
 
