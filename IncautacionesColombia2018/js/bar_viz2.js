1a318b3b65b2e1ecbd24348762fee5e2c7ab4a5d
Miguel Feijoo <ma.feijoo1120@uniandes.edu.co>
2018-09-25 17:16:01 -0500

    nuevo

--- IncautacionesColombia2018/js/bar_viz2.js
+++ IncautacionesColombia2018/js/bar_viz2.js
@@ -1,12 +1,6 @@
 
-var svg = d3.select( "svg.d" ),
-    margin = { top: 50, right: 30, bottom: 70, left: 80 },
-    iwidth = +svg.attr( "width" ) - margin.left - margin.right,
-    iheight = +svg.attr( "height" ) - margin.top - margin.bottom,
-    g = svg.append( "g" ).attr( "transform", "translate(" + margin.left + "," + margin.top + ")" );
+function grafica(depto,incautaciones_depto_sitio_mes,svg, g, iwidth, iheigth){
 
-
-function grafica(depto,incautaciones_depto_sitio_mes,svg){
 
 svg.selectAll(["id_1","id_2","id_3","id_4"]).remove();
 
@@ -174,7 +168,11 @@ shuffle(colors)
 
 function second(){
 
-
+var svg = d3.select( "svg.d" ),
+    margin = { top: 50, right: 30, bottom: 70, left: 80 },
+    iwidth = +svg.attr( "width" ) - margin.left - margin.right,
+    iheight = +svg.attr( "height" ) - margin.top - margin.bottom,
+    g = svg.append( "g" ).attr( "transform", "translate(" + margin.left + "," + margin.top + ")" );
 
 d3.csv('https://raw.githubusercontent.com/miguelfeijoo/IncautacionesColombia2018/master/DATOS/INCAUTACIONES_DEPARTAMENTO.csv', function ( d ) {
   return {
@@ -192,7 +190,7 @@ d3.csv('https://raw.githubusercontent.com/miguelfeijoo/IncautacionesColombia2018
   d3.select("#depto").on("change", function(){
                 depto = this.value;
 
- grafica(depto,incautaciones_depto_sitio_mes)
+ grafica(depto,incautaciones_depto_sitio_mes,svg, g, iwidth, iheigth)
 
 
 
