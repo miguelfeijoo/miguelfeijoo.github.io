2ea4d298f67d7ef9ce3f993632f2997326a2a838
Miguel Feijoo <ma.feijoo1120@uniandes.edu.co>
2018-10-18 15:15:33 -0500

    cambio

--- Delitos2018/js/group.js
+++ Delitos2018/js/group.js
@@ -1,7 +1,7 @@
 function first(){
 
                     
-				data = d3.merge(await Promise.all(["df_homicidios_total",'df_amenazas_total','df_delSexuales_total'].map(name => d3.csv(`https://raw.githubusercontent.com/miguelfeijoo/miguelfeijoo.github.io/master/Delitos2018/data/processed/${name}.csv`, d => {
+				d3.merge(await Promise.all(["df_homicidios_total",'df_amenazas_total','df_delSexuales_total'].map(name => d3.csv(`https://raw.githubusercontent.com/miguelfeijoo/miguelfeijoo.github.io/master/Delitos2018/data/processed/${name}.csv`, d => {
 				const date = parseDate(d["Fecha"]);
 
 				return {name: name.split("_")[1], date, value: +d["CantidadTotal"]};
